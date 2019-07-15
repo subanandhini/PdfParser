f1b0aaa20e5e7fa2b217c0f4903c16b3377248fa
Nandhini k <subanandhini08@gmail.com>
2019-07-15 12:57:25 +0530

    Uploading maven

---
+++ PdfParser/src/main/java/com/example/demo/PdfParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfParserApplication.class, args);
+	}
+
+}
